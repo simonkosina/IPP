3c999c5f40135c5aa6c1414a28799a37fa5dceaa
Simon Košina <simonkosina@gmail.com>
2021-02-26 08:22:28 +0100

    Komentare.

--- Stats.php
+++ Stats.php
@@ -1,21 +1,18 @@
 <?php
 
-
 class Stats
 {
-    // Pocitadla
-    private $loc = 0;
-    private $comments = 0;
-    private $fwjumps = 0;
-    private $backjumps = 0;
+    private $loc = 0; // pocet prikazov
+    private $comments = 0; // pocet comentarov
+    private $fwjumps = 0; // pocet skokov do predu
+    private $backjumps = 0; // pocet skokov do zadu
 
-    // Zoznamy
-    private $labels = array();
-    private $undefJumps = array();
+    private $labels = array(); // navestia
+    private $undefJumps = array(); // chybne skoky alebo skoky do predu
 
-    // Informacie o vystupnych suboroch
-    private $outputFiles = array();
+    private $outputFiles = array(); // subory pre vypis statistik
 
+    // $code - vstupny kod transformovany na zoznam prikazov
     public function setLoc($code) {
         $this->loc = count($code)-1;
     }
@@ -32,11 +29,16 @@ class Stats
         return $this->comments;
     }
 
+    // Zaznamenanie instrukcie skoku
+    // Ak sa navestie uz nachadza v zozname $labels, jedna sa o skok do zadu.
+    // V opacnom pripade je to bud skok do predu alebo chybne navestie.
+    // $label - navestie pouzite v instrukcii skoku
     public function addJump($label) {
-        // Navestie uz je definovane -> skok dozadu
         if (in_array($label, $this->labels, true) === true) {
+            // Navestie uz je definovane.
             $this->backjumps++;
-        } else { // Navestie nie je def. -> skok dopredu alebo chyba
+        } else {
+            // Navestie nie je definovane.
             array_push($this->undefJumps, $label);
         }
     }
@@ -50,13 +52,20 @@ class Stats
     }
 
     public function getBadJumps() {
+        // Pocet navesti v $undefJumps == Pocet zlych skokov
         return count($this->undefJumps);
     }
 
     public function getJumps() {
+        // jumps = backjumps + fwjumps + badjumps
         return ($this->getBackJumps() + $this->getFwJumps() + $this->getBadJumps());
     }
 
+    // Pridanie navestia do zoznamu $labels
+    // Ak dane navestie bolo v zozname $undefJumps,
+    // inkrementuje pocitadlo $fwjumps a navestia zo zoznamu odstrani.
+    // Nekontroluje duplikatne definicie navestia.
+    // $label - navestie pouzite v instrukcii LABEL
     public function addLabel($label) {
         // Ak dane navestia este nebolo definovane
         if (in_array($label, $this->labels, true) === false) {
@@ -76,34 +85,15 @@ class Stats
         return count($this->labels);
     }
 
+    // Ziska absolutnu cestu k suboru, kvoli kontrole
+    // identickych suborov zadanych roznym sposobom.
+    // Pre subor vytvori polozku v zozname $outputFiles, do
+    // ktorej priradi prazdne pole.
+    // $file - nazov suboru (rel., abs., meno)
     public function addFile($file) {
         global $err;
 
-        // Nazov suboru treba vzdy previest na absolutnu cestu,
-        // kvoli odhaleniu totoznych suborov zadanych inym sposobom.
-
-        $matches = array();
-        $matchCnt= 0;
-
-        // Vyber spravneho vyrazu
-        if (DIRECTORY_SEPARATOR == "\\") {
-            $pathPattern = "/.*\\\/";
-        } else {
-            $pathPattern = "/.*\\//";
-        }
-
-        $matchCnt = preg_match($pathPattern, $file, $matches); // abs./rel. cesta do adresara
-        $fileName = preg_replace($pathPattern, "", $file); // meno suboru
-
-        // Nastavenie abs. cesty k adresaru
-        if ($matchCnt == 1) {
-            $dirPath = realpath($matches[0]);
-        } else {
-            $dirPath = realpath('.');
-        }
-
-        // Nastavenie abs. cesty k suboru
-        $filePath = $dirPath.DIRECTORY_SEPARATOR.$fileName; // absolutna cesta k suboru
+        $filePath = $this->createFilePath($file);
 
         // Kontrola, ci dany subor uz bol pouzity
         if (!isset($this->outputFiles[$filePath])) {
@@ -114,15 +104,23 @@ class Stats
         }
     }
 
+    // Do zoznamy polozky $file v $outputFiles, prida
+    // nazov statistiky pre vypis.
+    // $param - statistika, kt. sa bude do suboru vypisovat
+    // $file - nazov suboru (rel., abs., meno)
     public function addParams2File($file, $param) {
-        if (isset($this->outputFiles[$file])) {
-            array_push($this->outputFiles[$file], $param);
+        $filePath = $this->createFilePath($file);
+
+        if (isset($this->outputFiles[$filePath])) {
+            array_push($this->outputFiles[$filePath], $param);
         }
     }
+
     public function getOutputFiles() {
         return $this->outputFiles;
     }
 
+    // Vrati zoznam obsahujuci hodnoty vsetkych statistik.
     public function getStatsArray() {
         $arr = [
             "--loc" => $this->getLoc(),
@@ -137,6 +135,7 @@ class Stats
         return $arr;
     }
 
+    // Do suborov v $outputFiles, vypise hodnoty prislusnych statistik.
     public function printStats() {
         global $err;
 
@@ -157,6 +156,35 @@ class Stats
 
             fclose($file);
          }
+    }
+
+    // Vrati absolutnu cestu k suboru.
+    // $file - nazov suboru (rel., abs., meno)
+    private function createFilePath($file) {
+        $matches = array();
+        $matchCnt= 0;
+
+        // Vyber spravneho reg. vyrazu
+        if (DIRECTORY_SEPARATOR == "\\") {
+            $pathPattern = "/.*\\\/";
+        } else {
+            $pathPattern = "/.*\\//";
+        }
+
+        $matchCnt = preg_match($pathPattern, $file, $matches); // abs./rel. cesta do adresara
+        $fileName = preg_replace($pathPattern, "", $file); // meno suboru
+
+        // Nastavenie abs. cesty k adresaru
+        if ($matchCnt == 1) {
+            $dirPath = realpath($matches[0]);
+        } else {
+            $dirPath = realpath('.');
+        }
+
+        // Nastavenie abs. cesty k suboru
+        $filePath = $dirPath.DIRECTORY_SEPARATOR.$fileName; // absolutna cesta k suboru
+
+        return $filePath;
     }
 }
 
