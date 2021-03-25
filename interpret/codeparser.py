ae6d111b4ac05fbc96b1fee2461f267513821feb
simonkosina <simonkosina@gmail.com>
2021-03-25 11:00:41 +0100

    Metoda parse().

--- interpret/codeparser.py
+++ interpret/codeparser.py
@@ -8,13 +8,62 @@ class CodeParser(object):
     Parsuje XML reprezentáciu kódu, vykonáva príslušné kontroly,
     operácie predáva na spracovanie instancii triedy CodeInterpret.
     
-    Atribúty:
+    Triedne atribúty:
+        opcodes (dict): operačný kód => zoznam argumentov 
+
+    Instančné atribúty:
         src_file (string): názov vstupného súboru
         xml_root (Element): koreň XML stromu
 
     Metody:
         readInput(self): číta vstup a získa koreň XML stromu
     """
+
+    opcodes = {
+        # ramce, volanie funkcii
+        "MOVE": ["var", "symb"],
+        "CREATEFRAME": list(),
+        "PUSHFRAME": list(),
+        "POPFRAME": list(),
+        "DEFVAR": ["var"],
+        "CALL": ["label"],
+        "RETURN": list(),
+        # zasobnik
+        "PUSHS": ["symb"],
+        "POPS": ["var"],
+        # operatore a konverzie
+        "ADD": ["var", "symb", "symb"],
+        "SUB": ["var", "symb", "symb"],
+        "MUL": ["var", "symb", "symb"],
+        "IDIV": ["var", "symb", "symb"],
+        "LT": ["var", "symb", "symb"],
+        "GT": ["var", "symb", "symb"],
+        "EQ": ["var", "symb", "symb"],
+        "AND": ["var", "symb", "symb"],
+        "OR": ["var", "symb", "symb"],
+        "NOT": ["var", "symb", "symb"],
+        "INT2CHAR": ["var", "symb"],
+        "STRI2INT": ["var", "symb", "symb"],
+        # vstup, vystup
+        "READ": ["var", "type"],
+        "WRITE": ["symb"],
+        # op. s retazcami
+        "CONCAT": ["var", "symb", "symb"],
+        "STRLEN": ["var", "symb"],
+        "GETCHAR": ["var", "symb", "symb"],
+        "SETCHAR": ["var", "symb", "symb"],
+        # op. s typmi
+        "TYPE": ["var", "symb"],
+        # riadenie toku programu
+        "LABEL": ["label"],
+        "JUMP": ["label"],
+        "JUMPIFEQ": ["label", "symb", "symb"],
+        "JUMPIFNEQ": ["label", "symb", "symb"],
+        "EXIT": ["symb"],
+        # ladiace instrukcie
+        "DPRINT": ["symb"],
+        "BREAK": list()
+        }
 
     def __init__(self, src_file = None):
         """
@@ -50,4 +99,19 @@ class CodeParser(object):
             print(f"Chybný XML formát vstupného súboru.\nRiadok: {err.position[0]}, Stĺpec: {err.position[1]}", file = sys.stderr)
             sys.exit(errors.XML_FORMAT)
 
-    
+    def parse(self):
+        """Parsuje vstupné XML. Kontroluje správnu štruktúru XML stromu.
+        Korektné inštrukcie predáva self.interpret na spracovanie."""
+
+        if self.xml_root.tag != "program":
+            print("Chybný tag koreňového elementu.", file = sys.stderr)
+            sys.exit(errors.XML_STRUCT)
+
+        if "language" not in self.xml_root.attrib or len(self.xml_root.keys()) != 1:
+            print("Chybný tag koreňového elementu.", file = sys.stderr)
+            sys.exit(errors.XML_STRUCT)
+        else:
+            if self.xml_root.attrib["language"] != "IPPcode21":
+                print("Chybný tag koreňového elementu.", file = sys.stderr)
+                sys.exit(errors.XML_STRUCT)
+            sys.exit(errors.XML_STRUCT)
--- interpret/interpret.py
+++ interpret/interpret.py
@@ -31,3 +31,4 @@ if __name__ == "__main__":
 
     parser = codeparser.CodeParser(src_file)
     parser.readInput()
+    parser.parse()
