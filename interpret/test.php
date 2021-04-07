3f08339f6d29a5b4ce9807820fe6b4ce90e4f202
simonkosina <simonkosina@gmail.com>
2021-04-07 11:35:05 +0200

    int test

--- interpret/test.php
+++ interpret/test.php
@@ -8,8 +8,48 @@ ini_set('display_errors', 'stderr');
 $options = parseArguments($argc, $argv);
 $options = setDefaultParams($options);
 
-var_dump($options);
+try {
+    $dir = new RecursiveDirectoryIterator($options["directory"]);
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+    exit(ERR_FOPEN_IN);
+}
 
-//$dir = new RecursiveDirectoryIterator()
 
+$iter = new RecursiveIteratorIterator($dir);
+$re_iter = new RegexIterator($iter,'/^.+\.src$/i',RecursiveRegexIterator::GET_MATCH);
+
+# interpret
+if (!$options["parse-only"]) {
+    foreach ($re_iter as $name) {
+        # vytvorenie prikazu
+        $cmd = "python3.8 ".$options["int-script"]." --source=".$name[0];
+        $file_no_ext = substr($name[0], 0, -4);
+
+        # pridanie inputu
+        if (!file_exists($file_no_ext.".in")) {
+            try {
+                $file = fopen($file_no_ext.".in", "w");
+                fclose($file);
+            } catch (Exception $e) {
+                echo $e->getMessage(), "\n";
+                exit(ERR_FOPEN_OUT);
+            }
+        }
+
+        # ocakavany rc
+        
+
+        $cmd = $cmd." --input=".$file_no_ext.".in";
+
+        echo $cmd."\n";
+
+        $out = array();
+        $rc = 0;
+
+        exec($cmd, $out, $rc);
+
+        # porovnanie rc
+    }
+}
 ?>
