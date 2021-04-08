9e54ee9f5c3b29c2bb050822e187b73a053deea6
Simon Košina <simonkosina@gmail.com>
2021-04-08 17:25:08 +0200

    css triedy

--- test/test.php
+++ test/test.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "test_errors.php";
+include_once "errors.php";
 include_once "parse_arguments.php";
 include_once "Test.php";
 include_once "IntTest.php";
@@ -38,4 +38,52 @@ if (!$options["parse-only"]) {
         echo $name." : ".$test->run()."\n";
     }
 }
+
+# HTML5 vystup
+$doc = new DOMDocument;
+$html = $doc->appendChild($doc->createElement("html"));
+$head = $html->appendChild($doc->createElement("head"));
+
+# meta info
+$node = $head->appendChild($doc->createElement("meta"));
+$node->setAttribute("charset", "UTF-8");
+
+# title
+$node = $head->appendChild($doc->createElement("title"));
+$node->nodeValue = "test.php";
+
+# header
+$header = $html->appendChild($doc->createElement("header"));
+$node = $header->appendChild($doc->createElement("h1"));
+$node->nodeValue = "Výsledky testov";
+
+# skripty
+$parse_id = "parse";
+$int_id = "interpret";
+
+$section = $html->appendChild($doc->createElement("section"));
+
+$title = $section->appendChild($doc->createElement("h2"));
+$title->nodeValue = "Testované skripty";
+
+$nav = $section->appendChild($doc->createElement("nav"));
+$ul = $nav->appendChild($doc->createElement("ul"));
+
+if (!$options["int-only"]) {
+    $li = $ul->appendChild($doc->createElement("li"));
+    $a = $li->appendChild($doc->createElement("a"));
+    $a->setAttribute("href", "#".$parse_id);
+    $a->nodeValue = $options["parse-script"];
+}
+
+if (!$options["parse-only"]) {
+    $li = $ul->appendChild($doc->createElement("li"));
+    $a = $li->appendChild($doc->createElement("a"));
+    $a->setAttribute("href", "#" . $int_id);
+    $a->nodeValue = $options["int-script"];
+}
+
+$doc->formatOutput = true;
+print $doc->saveHTML();
+
 ?>
